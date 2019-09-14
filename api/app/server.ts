50b5fe5171674ae12e421462d539fc2af7ac802d
Vitor Isaac <vitor2001bidi@gmail.com>
2019-09-14 16:32:15 -0300

    knex

--- api/app/server.ts
+++ api/app/server.ts
@@ -1,13 +1,19 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as bodyparser from "body-parser";
+import * as knex from 'knex';
+const options = require('../connection.js')
 
 const app = express();
 
-app.get('/', () => console.log(''));
+const database = knex(options)
+
+app.get('/', () => console.log('aaa'));
 
 
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('server started');
+    console.log(options);
+    database.from('Doenca');
 });
---
+++ api/connection.js
@@ -0,0 +1,12 @@
+const options = {
+    client: 'mysql2',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'senha',
+        database: 'mydb'
+    }
+}
+
+module.exports = options;
+
