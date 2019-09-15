fb9e02dca10dc29c85669870eeddec3727e762fc
Vitor Isaac <vitor2001bidi@gmail.com>
2019-09-14 23:18:59 -0300

    varias merdas

--- api/app/server.ts
+++ api/app/server.ts
@@ -3,6 +3,9 @@ import * as cors from "cors";
 import * as bodyparser from "body-parser";
 import * as knex from 'knex';
 import * as mysql from 'mysql';
+import * as https from 'https';
+import * as fs from 'fs';
+
 //const appRoutes = require('./app-routes/app.routes');
 import {appRoutes} from './app-routes/app.routes';
 import { webRoutes } from './web-routes/web.routes';
@@ -23,7 +26,13 @@ app.use('/app',appRoutes);
 
 app.use('/web', webRoutes)
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log('server started');
-    console.log(options);
-});
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+//   }, app)
+//   .listen(process.env.PORT || 8000, () => {
+//     console.log('server started');
+//     console.log(options);
+// });
+
+app.listen(8000);
