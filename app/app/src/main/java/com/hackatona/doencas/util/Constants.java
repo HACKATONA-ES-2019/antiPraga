4d8a3b5042b627aede2349c588316e4e17a1fb1d
joaocavajo <joao.santos.010@acad.pucrs.br>
2019-09-14 21:56:17 -0300

    remove app

--- app/app/src/main/java/com/hackatona/doencas/util/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.hackatona.doencas.util;
-
-public class Constants {
-
-    public static final String API_URL = "http://10.32.223.41:8000/";
-    public static final String REQUEST_SINTOMA = "app/sintomas";
-
-}
